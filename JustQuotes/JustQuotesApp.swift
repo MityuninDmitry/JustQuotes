28a6a85defe53779818c8f3da5a2cae95ffb0f6e
Mityunin Dmitry <mity-ml@mail.ru>
2023-07-01 22:57:53 +0500

    Initial Commit

---
+++ JustQuotes/JustQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustQuotesApp.swift
+//  JustQuotes
+//
+//  Created by Dmitry Mityunin on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct JustQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
